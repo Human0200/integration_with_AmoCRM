6dc6877b71ea8fee5ec1e402ecd4bde42529dd0f
Anton <ruant02@mail.ru>
2025-11-05 17:31:00 +0500

    правки

--- classes/FormAmoCRMHandler.php
+++ classes/FormAmoCRMHandler.php
@@ -2,8 +2,8 @@
 
 class FormAmoCRMHandler
 {
-    const AMOCRM_SUBDOMAIN = '31491690';
-    const AMOCRM_ACCESS_TOKEN = '';
+    const AMOCRM_SUBDOMAIN = 'pkintellekt';
+    const AMOCRM_ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQyNzE2ZTI1YzJkNzE2YWE2MzA1YmRiMjIwMTJhNWM1OWQ5NjJmNDUzNjc3YzMwMDY5MzQ5YzdlN2QxZmVhNTMzODVkNGFiMzNjMmMzYzVlIn0.eyJhdWQiOiI0MjY4ZTU3MC0xZGE0LTQ1NjktOTAyZi0yODBlYzVjMzQwODUiLCJqdGkiOiJkMjcxNmUyNWMyZDcxNmFhNjMwNWJkYjIyMDEyYTVjNTlkOTYyZjQ1MzY3N2MzMDA2OTM0OWM3ZTdkMWZlYTUzMzg1ZDRhYjMzYzJjM2M1ZSIsImlhdCI6MTc2MjM0MTI4OSwibmJmIjoxNzYyMzQxMjg5LCJleHAiOjE4OTYxMzQ0MDAsInN1YiI6IjExOTAzODEzIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNDkxNjkwLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMjE5OTNjZWUtNzE0OC00NWViLWE3Y2MtZDUyZmFhMzcyMDliIiwidXNlcl9mbGFncyI6MCwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.EAmzZf0cS2Asln6P5TeM9PGaCJgyFe36XTEvJo1PtihVK8HgqKlJmtJLiHMaGyP42G8t4FX0k3Vy83MKcQfa0h7ldncU_k5IOCJrPPlSmIlo_yq6Mqd1r0AdloNONijBr-xUKmvCM3jXi7wKf8X2BS2x_dxUvH9ONF5H1vl-y-cXMhn0zduejFraAOvVvyej90FwOTV64HIjXHV7Moz1nonY5cKIzVsabWD2hdeCEwOJb31FvcKTu68dM8wdSVcaLYgnYXN61fCnB0fJgo1HI_PyRre75JHrvS3noXDtQ-Kn58mF_RcNU0qlfws7R5xsOuzHKnBlp8iI6MnY9YRG_g';
     
     const RESPONSIBLE_USER_ID = 3673005;
     const PIPELINE_ID = 10131606;
@@ -12,7 +12,7 @@ class FormAmoCRMHandler
     public static function sendToAmoCRM($WEB_FORM_ID, $RESULT_ID)
     {
         try {
-            if (!\Bitrix\Main\Loader::includeModule('form')) {
+            if (!\CModule::IncludeModule('form')) {
                 return;
             }
             
@@ -78,49 +78,38 @@ class FormAmoCRMHandler
         $notes[] = 'Дата: ' . date('d.m.Y H:i:s');
         $notes[] = '---';
         
+        // Имя
+        if (!empty($formData['NAME'])) {
+            $contact['name'] = $formData['NAME'];
+            $lead['name'] = 'Заявка от ' . $formData['NAME'];
+        }
+        
+        // Телефон
+        if (!empty($formData['PHONE'])) {
+            $contact['custom_fields_values'][] = [
+                'field_code' => 'PHONE',
+                'values' => [
+                    ['value' => $formData['PHONE'], 'enum_code' => 'WORK']
+                ]
+            ];
+        }
+        
+        // Email
+        if (!empty($formData['EMAIL'])) {
+            $contact['custom_fields_values'][] = [
+                'field_code' => 'EMAIL',
+                'values' => [
+                    ['value' => $formData['EMAIL'], 'enum_code' => 'WORK']
+                ]
+            ];
+        }
+        
+        // Все остальные поля в комментарий
         foreach ($formData as $fieldCode => $fieldValue) {
-            if (empty($fieldValue)) {
+            if (in_array($fieldCode, ['NAME', 'PHONE', 'EMAIL']) || empty($fieldValue)) {
                 continue;
             }
             
-            $fieldCodeUpper = strtoupper($fieldCode);
-            
-            // Имя
-            if (stripos($fieldCode, 'NAME') !== false || 
-                stripos($fieldCode, 'IMY') !== false || 
-                stripos($fieldCode, 'IMYA') !== false) {
-                $contact['name'] = $fieldValue;
-                $lead['name'] = 'Заявка от ' . $fieldValue;
-                continue;
-            }
-            
-            // Телефон
-            if (stripos($fieldCode, 'PHONE') !== false || 
-                stripos($fieldCode, 'TEL') !== false ||
-                stripos($fieldCode, 'TELEFON') !== false) {
-                $contact['custom_fields_values'][] = [
-                    'field_code' => 'PHONE',
-                    'values' => [
-                        ['value' => $fieldValue, 'enum_code' => 'WORK']
-                    ]
-                ];
-                continue;
-            }
-            
-            // Email
-            if (stripos($fieldCode, 'EMAIL') !== false || 
-                stripos($fieldCode, 'MAIL') !== false ||
-                stripos($fieldCode, 'E-MAIL') !== false) {
-                $contact['custom_fields_values'][] = [
-                    'field_code' => 'EMAIL',
-                    'values' => [
-                        ['value' => $fieldValue, 'enum_code' => 'WORK']
-                    ]
-                ];
-                continue;
-            }
-            
-            // Остальное в комментарий
             $notes[] = $fieldCode . ': ' . $fieldValue;
         }
         
--- init.php
+++ init.php
@@ -2,11 +2,8 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/FormAmoCRMHandler.php');
 
-use Bitrix\Main\EventManager;
 
-$eventManager = EventManager::getInstance();
-$eventManager->addEventHandler(
-    'form',
-    'onAfterResultAdd',
-    ['FormAmoCRMHandler', 'sendToAmoCRM']
-);
+AddEventHandler('form', 'onAfterResultAdd', ['FormAmoCRMHandler', 'sendToAmoCRM']);
+
+
+AddEventHandler('sale', 'OnSaleOrderSaved', ['FormAmoCRMHandler', 'sendOrderToAmoCRM']);
